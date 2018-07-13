0525079b4dbc7dddb7da734657c4c37f90fd5c68
minami-choi <cmina2846@gmail.com>
2018-07-13 16:46:31 +0900

    chain so 테스트중 ( send_tx api 동작안함)

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -14,11 +14,11 @@ export class AppComponent implements OnInit {
   }
 
   ngOnInit() {
-    this.createRawTx()
+    // this.createRawTx()
     // this.getInfo('DOGE')
 
     // this.getBalance('BTC', '12uhXixxk1TQMimYimjMr4WTQLtf4QeQLt')
-    // this.getReceivedValue('BTC', '12uhXixxk1TQMimYimjMr4WTQLtf4QeQLt')
+    // // this.getReceivedValue('BTC', '12uhXixxk1TQMimYimjMr4WTQLtf4QeQLt')
     // this.getUnspentTx('BTC', '12uhXixxk1TQMimYimjMr4WTQLtf4QeQLt')
     // this.isValidAddr('BTC', '12uhXixxk1TQMimYimjMr4WTQLtf4QeQLt')
     // this.getAllData('BTC', '12uhXixxk1TQMimYimjMr4WTQLtf4QeQLt')
@@ -29,7 +29,7 @@ export class AppComponent implements OnInit {
     // this.getTxOutputs('BTC', '5fb57e59919a08623e4e55a9f68a328200528a74831069ea9ff6b90a92e1693f')
     // this.getTx('BTC', '5fb57e59919a08623e4e55a9f68a328200528a74831069ea9ff6b90a92e1693f')
     // this.isTxConfirmed('BTC', '5fb57e59919a08623e4e55a9f68a328200528a74831069ea9ff6b90a92e1693f')
-    this.isTxSpent('BTC', '5fb57e59919a08623e4e55a9f68a328200528a74831069ea9ff6b90a92e1693f')
+    // this.isTxSpent('BTC', '4445a12e079eaf56653f59b8330b0b291a46c72b0fa6e2caae990bc0ad77a566')
     // this.getTxDisplayData('BTC', '5fb57e59919a08623e4e55a9f68a328200528a74831069ea9ff6b90a92e1693f')
 
 
@@ -43,7 +43,7 @@ export class AppComponent implements OnInit {
   }
 
   createRawTx() {
-    this.bitcoinService.createRawTx();
+    this.bitcoinService.createRawTx('LTC');
   }
 
   // network info
@@ -205,15 +205,18 @@ export class AppComponent implements OnInit {
 
   // send Transaction
   sendTx() {
-    
     const body = {
-      tx_hex: ''
+      tx_hex: '01000000010fcc747bf362b576710fac24170bf3777748000a4aca44ae47accccf39393ba6010000006b4830450221009f16cb0de23a95520bd59f403bbe3977e782cb218b6baf820099abdf751db7c6022009451152dc59e3fc943948fb01e0ac53a18cb92cdc8efe38cc17a4077ddbd4c401210387ed691264a611ad6f33622df5b6b3a31c406c8edeb16d66029a7accf269528affffffff027d423d05000000001976a91426d41b49acc3d8b974ed46e25dceaa6101f6094788ac400d0300000000001976a914b8f40e091f8b05026cf092d1a436b228b5942f2788ac00000000'
     }
-    // this.sendTx('BTC', body)
-    this.chainso.sendTx('BTC', body).subscribe(res => {
+    this.chainso.sendTx('LTC', body).subscribe(res => {
       console.log("sendTx", res)
     }, err => {
       console.log("err", err)
     })
+  }
+
+  getRawTx() {
+    const txid = "f9a392270c5d3f497320e41da53de09676da5c47c9be0c8dcd99c1137a70a99b"
+    this.bitcoinService.getRawTx(txid)
   }
 }
--- src/app/bitcoinjs.service.ts
+++ src/app/bitcoinjs.service.ts
@@ -5,14 +5,17 @@ import * as bitcoinjs from 'bitcoinjs-lib'
 export class BitcoinjsService {
 
     bitcoinNetwork: any;
+    ltcNetwork: any;
 
     constructor() {
         this.bitcoinNetwork = bitcoinjs.networks.bitcoin
+        this.ltcNetwork = bitcoinjs.networks.litecoin
     }
 
-    createRawTx() {
-
-        const key = bitcoinjs.ECPair.fromWIF("privatekey", this.bitcoinNetwork);
+    createRawTx(coin) {
+        let network;
+        coin === 'BTC' ? network = this.bitcoinNetwork : network = this.ltcNetwork;
+        const key = bitcoinjs.ECPair.fromWIF("private key", network);
         // get pubkey hash for P2WPKH
         const pubKeyHash = bitcoinjs.crypto.hash160(key.getPublicKeyBuffer());
         // create P2WPKH for use as redeemScript
@@ -20,19 +23,38 @@ export class BitcoinjsService {
         
         console.log("createRawTx",pubKeyHash,  key)
 
-        const tx = new bitcoinjs.TransactionBuilder(this.bitcoinNetwork);
-        console.log("tx", tx)
-        tx.setVersion(2); // version 1, 2?? 차이
-
-        tx.addInput("5fb57e59919a08623e4e55a9f68a328200528a74831069ea9ff6b90a92e1693f", 1);
-        tx.addOutput("12uhXixxk1TQMimYimjMr4WTQLtf4QeQLt", 569890);
+        const tx = new bitcoinjs.TransactionBuilder(network);
+        // console.log("tx", tx)
+        tx.setVersion(1);
+        tx.addInput("a63b3939cfccac47ae44ca4a0a00487777f30b1724ac0f7176b562f37b74cc0f", 1);
+        tx.addOutput("LNmG2viicYJZMovnX3ciuQSbxBuhKcSsJs", 87900797);
         // added more to the change, as "too big fee" warning was triggered
-        tx.addOutput("184V95i7AD1xjFG7kf1X5Sz55cE2rXAHPo", 200000);
+        tx.addOutput("Lc5txjffwkcbMk5WyF1qhB4WL5vHL7i8xv", 200000);
 
         // include the value of the input and the redeemScript
         // vin, keyPair, redeemScript, hashType, witnessValue, witnessScript
         tx.sign(0, key );
 
-        console.log(tx.build().toHex(), tx.buildIncomplete().toHex());
+        console.log(tx.build().toHex());
+        // 서명되기 전 rawTx
+        //  tx.buildIncomplete().toHex() 
+    }
+
+
+    getRawTx(txid) {
+        const res = bitcoinjs.Transaction.toHex(txid)
+        console.log("getRawTx", res)
+    }
+
+    decode(rawTx) {
+        const tx = bitcoinjs.Transaction.fromHex(rawTx);
+        const txid = tx.getId();
+        console.log(txid);
     }
 }
+
+// wannabit
+// 020000000166a577adc00b99aecae2a60f2bc7461a290b0b33b8593f6556af9e072ea14544010000006b483045022100924023e9886b02613df323c35184cd5a43d3cfd47296ae8c16106404e2db426b022023dfd5b88aee369112d32b41cdfeb7d00afae9f456a75a2d64829830ffcbc403012103baa3aafb68a5ae6a808e82c3111a4d89535107be0b1f651750e6e19465e24469ffffffff02400d0300000000001976a9144d719e2ed6f936a6f99b39d2e0d7c3fc44e5120e88ac069f0500000000001976a91414ef969ad4261cb0719fb97d500c779f1fb5c51988ac00000000
+
+// bitcoinjs-lib
+// 020000000166a577adc00b99aecae2a60f2bc7461a290b0b33b8593f6556af9e072ea14544010000006a47304402207cc30897d284aa0ed14796e58d5471e25efb481e639924cf29694d627891ee8702205aa8daeb3f300419670d84dfa1fd2a07d2f4f17a93e42175964dcfcfed359def012103baa3aafb68a5ae6a808e82c3111a4d89535107be0b1f651750e6e19465e24469ffffffff02069f0500000000001976a91414ef969ad4261cb0719fb97d500c779f1fb5c51988ac400d0300000000001976a9144d719e2ed6f936a6f99b39d2e0d7c3fc44e5120e88ac00000000
